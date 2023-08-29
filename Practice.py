074dd9db0dfa6bda02b44b291892687e3f4d8a81
rezadrakhshan <130975344+rezadrakhshan@users.noreply.github.com>
2023-08-29 11:19:17 +0330

    add my practice

---
+++ Practice.py
@@ -0,0 +1,20 @@
+#-----1-----
+# base = int(input("enter base:\n"))
+# height = int(input("enter height:\n"))
+# area = base * height
+
+# print(f"area = {area}")
+
+#-----2-----
+# height = int(input("enter height:\n"))
+# radian = int(input("enter radian:\n"))
+# volume = height * (radian*radian) * 3
+# sur_area = 2 * 3 * radian * height + 2 * (3 * (radian * radian))
+
+# print(f"volume:{volume}\nsur area:{sur_area}")
+
+#-----3-----
+# radian = int(input("enter radion:\n"))
+# sur_area = 4 * 3 * (radian * radian)
+# volume = (4/3) * 3 * (radian * radian)
+# print(f"sur area:{sur_area}\nvolume:{volume}")
