88d4158dee5ab8859b5acd31ddd2d358653819de
RezaDerakhshan <130975344+rezadrakhshan@users.noreply.github.com>
2023-12-01 22:37:58 +0330

    Add String Methods

---
+++ String Methods.py
@@ -0,0 +1,15 @@
+text = 'Hello World'
+print(text.capitalize()) # --->  Hello world
+print(text.casefold()) # --->  hello world
+print(text.count('o')) # --->  2
+print(text.find('World')) # --->  6
+print(text.index('Hello')) # --->  0
+print(text.isalnum()) # --->  False
+print(text.isalpha()) # --->  False
+print(text.isascii()) # --->  True
+print(text.isdecimal()) # --->  False
+print(text.isdigit()) # ---> False
+print(text.isidentifier()) # --->  False
+print(text.islower()) # --->  True
+print(text.isnumeric()) # --->  False
+print(text.isprintable()) # --->  True
